9b539c45a8a114d01a1af48f9bbb235d45e70ea2
ivan-stankevich <stankevichivann@yandex.by>
2023-01-02 22:24:54 +0100

    initCommit

---
+++ src/test/java/com/springjunit/SpringJUnitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springjunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJUnitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
